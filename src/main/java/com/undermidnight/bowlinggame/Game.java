ff3e59eed1ee67930fea55a7be27daf262258cd8
Undermidnight <jhissong@gmail.com>
2016-03-02 20:39:32 -0500

    Initial test case creation

---
+++ src/main/java/com/undermidnight/bowlinggame/Game.java
@@ -0,0 +1,5 @@
+package com.undermidnight.bowlinggame;
+
+public class Game {
+
+}
---
+++ src/test/java/com/undermidnight/bowlinggame/BowlingGameTest.java
@@ -0,0 +1,9 @@
+package com.undermidnight.bowlinggame;
+
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+	public void testGutterGame() throws Exception {
+		Game g = new Game();
+	}
+}
